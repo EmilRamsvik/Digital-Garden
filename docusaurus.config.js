2a49c36e3230d53aac91cc1d667f32cf09e7ce0e
Emil Ramsvik Mac Home <emil.ramsvik@wilhelmsen.com>
2024-08-20 21:51:13 +0200

    Added correct site name which fixes bug with link but also might fix search (i hope)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://tinyknowledge.com',
   baseUrl: '/',
 
   onBrokenLinks: 'ignore',
